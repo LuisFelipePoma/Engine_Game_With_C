f26562b493cd85f2f4dc795838059516ae97815f
Felipe <u202110902@upc.edu.pe>
2023-05-04 20:13:46 -0500

    Integracion de la rutina para el agregado de sprites

--- Motor/MainGame.cpp
+++ Motor/MainGame.cpp
@@ -1,6 +1,7 @@
 #include "MainGame.h"
 #include <iostream>
 #include "Error.h"
+
 using namespace std;
 
 MainGame::MainGame() {
@@ -9,6 +10,14 @@ MainGame::MainGame() {
 	gameState = GameState::PLAY;
 	time = 0;
 	sprites = new Sprites();
+
+	// Variables e inicializaciones para generar posiciones aleatorias
+	gen = mt19937(rd());
+	dist = uniform_real_distribution<float>(-1.0f, 1.0f);
+
+	// Variables e inicializaciones para la creacion del intervalo
+	intervalo = 2500;
+	inicio = chrono::steady_clock::now();
 }
 
 MainGame::~MainGame() {
@@ -65,16 +74,24 @@ void MainGame::draw() {
 	window.swapWindow();
 }
 
+void MainGame::rutinaSprites()
+{
+	this_thread::sleep_until(inicio + chrono::milliseconds(intervalo)); // Detiene la funcion durante un tiempo
+	sprites->insertSprites(dist(gen), dist(gen), 1, 1, "Textures/gato.png"); // Inicializa al sprite y lo agrega al vector
+	sprites->initSprites();// Inicializa los Sprites
+	inicio += chrono::milliseconds(intervalo); // Reinicia la rutina
+}
+
 void MainGame::run() {
 	init();
-	sprites->insertSprites(-1, -1, 1, 1, "Textures/gato.png");
-	sprites->insertSprites(0, 0, 1, 1, "Textures/mario.png");
-	sprites->initSprites();
 	update();
 }
 
 void MainGame::update() {
+	
+
 	while (gameState != GameState::EXIT) {
+		rutinaSprites();
 		draw();
 		processInput();
 	}
--- Motor/MainGame.h
+++ Motor/MainGame.h
@@ -1,6 +1,11 @@
 #pragma once
 #include <SDL/SDL.h>
 #include <GL/eglew.h>
+//Librerias necesarias para la rutina
+#include <chrono>
+#include <thread>
+#include <random>
+//----------------------------------
 #include "Sprites.h"
 #include "HLSLProgram.h"
 #include "Window.h"
@@ -12,6 +17,15 @@ enum class GameState {
 class MainGame
 {
 private:
+	//Variables para la rutina
+	int intervalo;
+	chrono::steady_clock::time_point inicio;
+	mt19937 gen;
+
+	//Variables para generar la posicion aleatoria
+	random_device rd;
+	uniform_real_distribution<float> dist;
+
 	int width;
 	int height;
 	float time;
@@ -26,7 +40,7 @@ public:
 	MainGame();
 	~MainGame();
 	GameState gameState;
-
+	void rutinaSprites(); // Se crea esta funcion adicional para la rutina
 	void run();
 	void draw();
 	void update();
